51541af898639d83dd63053a9d6f60bd98c20816
Alexey Sudachen <alexey@sudachen.name>
2008-02-25 13:57:55 +0300

    ?

--- jungle/_lzss.cmd
+++
@@ -1,1 +0,0 @@
-cl -LD -MD -Ox _lzss.c -Ic:\Visus\Python\include c:\Visus\Python\Libs\python24.lib -Fe_lzss.pyd
--- jungle/_lzss.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-P=`python -c 'import sys; print sys.version[:3]'`
-gcc -shared -O2 _lzss.c -I /usr/include/python$P -o _lzss.so -lpython$P
